ac9588f9cb67c085a0309de539b7350a6a955f83
고동우 <ehddn2202@naver.com>
2021-05-28 02:53:44 +0900

    전투UI추가중

--- Source/test12/Enemy.h
+++ Source/test12/Enemy.h
@@ -27,4 +27,16 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,Category="Enemy_property")
+		FString Name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy_property")
+		int Type;    //속성, 비교하기 편하게 인수형으로 할 생각
+	//URPOPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy_property")
+	//체력 만들 예정. 지금은 블루프린트화
+
+
+
+
 };
